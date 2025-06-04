1b1ada65f1b69c07900f9dc25b3a60bc77bb7755
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-06-04 07:00:47 +0300

    Add TomItemModel

---
+++ app/src/main/java/com/asoom/tomandjerry/ui/screens/TomItemModel.kt
@@ -0,0 +1,9 @@
+package com.asoom.tomandjerry.ui.screens
+
+data class TomItemModel(
+    val imageId: Int,
+    val title: String,
+    val description: String,
+    val price: Int,
+    val salePrice: Int,
+)
